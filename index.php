556878d9aeece39746e5f95a6a1f3289b20be508
megtaho <122614093+megtaho@users.noreply.github.com>
2023-03-30 15:23:04 +0200

    modif index.php

--- index.php
+++ index.php
@@ -228,7 +228,7 @@
 
         <header class="section-header">
           <h2>Contact</h2>
-          <p>Contact Us</p>
+          <p>Contactez nous</p>
         </header>
 
         <div class="row gy-4">
@@ -239,29 +239,29 @@
               <div class="col-md-6">
                 <div class="info-box">
                   <i class="bi bi-geo-alt"></i>
-                  <h3>Address</h3>
-                  <p>A108 Adam Street,<br>New York, NY 535022</p>
+                  <h3>Adresse</h3>
+                  <p>120 rue de la lecture <br>Lyon, 69123</p>
                 </div>
               </div>
               <div class="col-md-6">
                 <div class="info-box">
                   <i class="bi bi-telephone"></i>
-                  <h3>Call Us</h3>
-                  <p>+1 5589 55488 55<br>+1 6678 254445 41</p>
+                  <h3>Appelez nous</h3>
+                  <p>+33 9 43 42 67 78<br>+33 9 43 42 67 78</p>
                 </div>
               </div>
               <div class="col-md-6">
                 <div class="info-box">
                   <i class="bi bi-envelope"></i>
-                  <h3>Email Us</h3>
+                  <h3>Envoyez nous un mail</h3>
                   <p>info@example.com<br>contact@example.com</p>
                 </div>
               </div>
               <div class="col-md-6">
                 <div class="info-box">
                   <i class="bi bi-clock"></i>
-                  <h3>Open Hours</h3>
-                  <p>Monday - Friday<br>9:00AM - 05:00PM</p>
+                  <h3>Heures d'ouverture</h3>
+                  <p>Lundi - Vendredi<br>9h - 17h</p>
                 </div>
               </div>
             </div>
